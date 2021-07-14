03387dc0021f77165bda530a09772840fd996217
carlo33 <cyaco33@gmail.com>
2021-07-14 20:42:39 +0000

    uses module services - class Session- login and logout

---
+++ lib/services/session.rb
@@ -0,0 +1,31 @@
+require "json"
+require "httparty"
+
+module Services
+  class Session
+    include HTTParty
+    base_uri "https://expensable-api.herokuapp.com/"
+
+    def self.login(login_data)
+      options = {
+        headers: { "Content-Type": "application/json" },
+        body: login_data.to_json
+      }
+      response = post("/login", options)
+      raise HTTParty::ResponseError, response unless response.success?
+
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def self.logout
+      options = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Token token=#{token}"
+        }
+      }
+      response = post("/logout", options)
+      raise HTTParty::ResponseError, response unless response.success?
+    end
+  end
+end
