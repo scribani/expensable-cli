b70ba84349cd9ffcbf3fa045c44dbed39dd81da6
Scribani <scribani.andres@gmail.com>
2021-07-15 03:54:37 +0000

    Define a method inside transaction module for updating a transaction

--- lib/handlers/transactions.rb
+++ lib/handlers/transactions.rb
@@ -1,14 +1,22 @@
 module Codeable
   module Handlers
     module Transactions
-      def add_transaction(id)
-        transaction_data = Helpers::Requester.transaction_form
-        transaction_response = Services::Transaction.create(transaction_data, @token, id)
-        found_category = @categories.find(&id_look_up(id))
+      def append_to_transactions(transaction_response)
+        found_category = @categories.find { |category| category[:id] == id }
         found_category[:transactions] << transaction_response
       end
 
-      def update_transaction; end
+      def add_transaction(category_id)
+        transaction_data = Helpers::Requester.transaction_form
+        transaction_response = Services::Transaction.create(transaction_data, @token, category_id)
+        append_to_transactions(transaction_response)
+      end
+
+      def update_transaction(category_id, transaction_id)
+        transaction_data = Helpers::Requester.transaction_form
+        transaction_response = Services::Transaction.update(transaction_data, category_id, @token, transaction_id)
+        append_to_transactions(transaction_response)
+      end
 
       def delete_transaction(id); end
 
