e1619145212240c28f5e087b049bd807447df58b
Scribani <scribani.andres@gmail.com>
2021-07-15 03:57:35 +0000

    Define a method inside transaction module for deleting a transaction

--- lib/handlers/transactions.rb
+++ lib/handlers/transactions.rb
@@ -1,7 +1,7 @@
 module Codeable
   module Handlers
     module Transactions
-      def append_to_transactions(transaction_response)
+      def append_to_transactions(id, transaction_response)
         found_category = @categories.find { |category| category[:id] == id }
         found_category[:transactions] << transaction_response
       end
@@ -9,16 +9,19 @@ module Codeable
       def add_transaction(category_id)
         transaction_data = Helpers::Requester.transaction_form
         transaction_response = Services::Transaction.create(transaction_data, @token, category_id)
-        append_to_transactions(transaction_response)
+        append_to_transactions(category_id, transaction_response)
       end
 
       def update_transaction(category_id, transaction_id)
         transaction_data = Helpers::Requester.transaction_form
         transaction_response = Services::Transaction.update(transaction_data, category_id, @token, transaction_id)
-        append_to_transactions(transaction_response)
+        append_to_transactions(category_id, transaction_response)
       end
 
-      def delete_transaction(id); end
+      def delete_transaction(category_id, transaction_id)
+        Services::Transaction.destroy(category_id, @token, transaction_id)
+        @categories.delete_if { |category| category[:id] == category_id }
+      end
 
       def calculate_total; end
     end
