aa895c6d4ad2f70ad53a704119b051638cc05d96
carlo33 <cyaco33@gmail.com>
2021-07-15 02:25:05 +0000

    test for Module Services class Category (create-update-delete)

---
+++ lib/services/category_test.rb
@@ -0,0 +1,60 @@
+require "minitest/autorun"
+require_relative "./category"
+require_relative "./session"
+
+class CategoryServiceTest < Minitest::Test
+  def setup
+    @data = {
+      name: "salud123",
+      transaction_type: "expense"
+    }
+    @data_update = {
+      name: "salud y medicina"
+    }
+    @data_login = {
+      email: "test24@mail.com",
+      password: "123456"
+    }
+    @token = obtein_token
+  end
+
+  def obtein_token
+    user_login = Services::Session.login(@data_login)
+    user_login[:token]
+  end
+
+  def test_list_call
+    response = Services::Category.list(@token)
+    assert_instance_of Array, response
+    assert_includes response.sample.keys, :id
+    assert_includes response.sample.keys, :name
+    assert_includes response.sample.keys, :transaction_type
+    assert_includes response.sample.keys, :transactions
+  end
+
+  def test_create_delete_call
+    response = Services::Category.create(@data, @token)
+    assert_instance_of Hash, response
+    assert_includes response.keys, :id
+    assert_includes response.keys, :name
+    assert_includes response.keys, :transaction_type
+    assert_includes response.keys, :transactions
+    id = response[:id]
+    update_call id
+  end
+
+  def update_call(id)
+    response = Services::Category.update(@data_update, id, @token)
+    assert_includes response.keys, :id
+    assert_includes response.keys, :name
+    assert_equal response[:name], @data_update[:name]
+    assert_includes response.keys, :transaction_type
+    assert_includes response.keys, :transactions
+    response = Services::Category.destroy(id, @token)
+    assert_nil response
+  end
+
+  def test_search_call
+    skip
+  end
+end
