71c3f54efde3c538a158eac7c7a576e7cbcfe6d2
Scribani <scribani.andres@gmail.com>
2021-07-15 10:11:47 +0000

    Create test for create_category in categories module

---
+++ lib/handlers/categories_test.rb
@@ -0,0 +1,41 @@
+require "httparty"
+require "minitest/autorun"
+require_relative "./categories"
+require_relative "../io_test_helpers"
+require_relative "../helpers/requester"
+require_relative "../services/category"
+require_relative "../services/transaction"
+
+class CategoriesHandlersTest < Minitest::Test
+  include Handlers::Categories
+  include IoTestHelpers
+  include HTTParty
+  base_uri "https://expensable-api.herokuapp.com"
+
+  def setup
+    @token = nil
+    @categories = []
+  end
+
+  def clean_category_after_test(id)
+    self.class.delete("/categories/#{id}", headers: { Authorization: "Token token=#{@token}" })
+  end
+
+  def test_create_category_correctly_adds_new_category_to_list
+    inputs = ["Test Transaction", "income"]
+
+    capture_io do
+      simulate_stdin(*inputs) do
+        create_category
+      end
+    end
+    id = @categories.last[:id]
+    clean_category_after_test(id)
+
+    assert_instance_of Hash, @categories.last
+    assert_includes @categories.last.keys, :id
+    assert_includes @categories.last.keys, :name
+    assert_includes @categories.last.keys, :transaction_type
+    assert_includes @categories.last.keys, :transactions
+  end
+end
--- lib/handlers/session.rb
+++ lib/handlers/session.rb
@@ -2,17 +2,17 @@ module Codeable
   module Handlers
     module Session
       def create_user
-        user_data = Codeable::Helpers::Requester.login_user
+        user_data = Codeable::Helpers::Requester.create_user
         user_response = Codeable::Services::User.create(user_data)
         @token = user_response[:token]
-        @username = user_response[:username]
+        @email = user_response[:email]
       end
 
       def login
         login_data = Codeable::Helpers::Requester.login_user
         login_response = Codeable::Services::Session.login(login_data)
         @token = login_response[:token]
-        @username = login_response[:username]
+        @email = login_response[:email]
       end
     end
   end
