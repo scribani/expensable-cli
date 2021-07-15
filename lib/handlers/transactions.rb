32930497daa8ceeefb8c48625c6cb52d7842cd6a
Scribani <scribani.andres@gmail.com>
2021-07-15 03:48:30 +0000

    Define a method inside transaction module for adding a transaction to a category

--- lib/handlers/transactions.rb
+++ lib/handlers/transactions.rb
@@ -1,7 +1,12 @@
 module Codeable
   module Handlers
     module Transactions
-      def add_transaction; end
+      def add_transaction(id)
+        transaction_data = Helpers::Requester.transaction_form
+        transaction_response = Services::Transaction.create(transaction_data, @token, id)
+        found_category = @categories.find(&id_look_up(id))
+        found_category[:transactions] << transaction_response
+      end
 
       def update_transaction; end
 
